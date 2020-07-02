8bf52840f948215bc57858101842ae5fd1e84782
BoyPurba <boypoerbah@gmail.com>
2020-07-02 12:48:42 +0700

    update stack and push

--- resources/views/items/data-tables.blade.php
+++ resources/views/items/data-tables.blade.php
@@ -442,3 +442,15 @@
     </div>
 
     @endsection
+
+    @push('scripts')
+
+    <script src="{{asset('/adminlte/plugins/datatables/jquery.dataTables.js')}}"></script>
+    <script src="{{asset('/adminlte/plugins/datatables-bs4/js/dataTables.bootstrap4.js')}}"></script>
+    <script>
+        $(function () {
+        $("#example1").DataTable();
+        });
+    </script>
+
+    @endpush
